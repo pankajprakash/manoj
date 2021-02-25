3596bb2604d41e780f6e412562e6d6f0fe5a1b92
pankajprakash <pankajprakash25@gmail.com>
2021-02-25 14:45:21 -0500

    jksa

--- file.js
+++ file.js
@@ -60,3 +60,73 @@ function deleterecord(id)
             console.log(err);
         });
     }
+
+
+
+
+
+    function form_show(id)
+{
+    let value=id.data[0];
+    let i=id.data[0].id;
+    console.log(value.name);
+    let form="";
+    
+            form +=" <form id='form_table'>";
+            form +="<label for=''>Name &nbsp;&nbsp;&nbsp;&nbsp;</label>";
+            form +=`<input type='text' name='name' id='name' value="${value.name}" class='input_post_form' placeholder='' ><br><br>`;
+
+            form +="<label for=''>Email Id </label>";
+            form +="<input type='text' name='email' id='email' value="+value.email+" class='input_post_form' placeholder=''><br><br>";
+
+            form +="<label for=''>Address&nbsp;</label>";
+            form +="<input type='text' name='address' id='address' value="+value.address+" class='input_post_form'><br><br>";
+
+            form +="<label for=''>Phone &nbsp;&nbsp;&nbsp;</label>";
+            form +="<input type='text' name='phone' id='phone' value="+value.phone+" class='input_post_form'><br><br>";
+
+            form +="<label for=''>Gender&nbsp;&nbsp;</label>";
+            form +="<input type='text' name='gender' id='gender' value="+value.gender+" class='input_post_form'><br><br>";
+
+            form +="<label for=''>Country&nbsp;</label>";
+            form +="<input type='text' name='country' id='country' value="+value.country+" class='input_post_form'><br><br>";
+
+            form +="<label for=''>state  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</label>";
+            form +="<input type='text' name='state' id='state' value="+value.state+" class='input_post_form'><br><br>";
+            form +="<input type='submit' name='name' id='' value='submit'>"
+            form +="</form>";
+            
+            document.getElementById("body").innerHTML=form;
+        form_table.onsubmit = async (e) => {
+            e.preventDefault();
+            
+             let name=document.getElementById("name").value;
+             let email=document.getElementById("email").value;
+             let address=document.getElementById("address").value;
+             let phone=document.getElementById("phone").value;
+            let gender=document.getElementById("gender").value;
+             let country=document.getElementById("country").value;
+             let state=document.getElementById("state").value;
+
+            alert(name);
+
+           let response = await fetch('http://192.168.1.10:80/api/user/update/'+i,{ method: 'PUT' ,  body: JSON.stringify({ 
+             "name":name,
+            "email":email,
+           "address" : address,
+            "phone":phone,
+            "gender":gender,
+         "country" :country,
+            "state": state
+        }),
+             headers: { 
+                "Content-type": "application/json;"
+            } })
+
+             let result = await response.json();
+
+            alert(result.message);
+         };
+
+       
+}
--- practice.js
+++ practice.js
@@ -30,7 +30,7 @@ tbl+=`<tr>
 <td><button type="button"  id="${res[i].id}"  onclick="cwlick(${i})" class="btn btn-success">View</button>
 <button type="button" id="${res.id}" onclick="create_data(${i})" class="btn btn-secondary" >Create</button>
 <button type="button" id="${res.id}" onclick="deleterecord(${i})" class="btn btn-danger"   >Delete</button>
-<button type="button" id="${res.id}" onclick="updaterecord(${i})" class="btn btn-primary" >Update</button></td>
+<button type="button" id="${res.id}" onclick="disp(${i})" class="btn btn-primary" >Update</button></td>
 </tr>
 `
   
@@ -48,7 +48,7 @@ console.log(id)
     fetch(" http://192.168.1.10:80/api/users/", {"method":"GET"})
     .then((res)=> res.json())
     .then( (apidata) =>{
-let res= apidata.result;  
+ res= apidata.result;  
 
 var table=` <tr>
 <td>`+res[id].id+`</td>
@@ -135,15 +135,102 @@ function deleterecord(id)
         });
     }
 
-function updaterecord(id)
+
+
+function disp(id)
 {
-    let change=res[id].id;
-    fetch(' http://192.168.1.10:80/api/user/update/5'+change,{method: 'PUT'})
-    .then ((response)=>response.json())
-    .then ((data)=>console.log(data))
-    .catch(function (err){
-        console.log(err);
+    let key=res[id].id;
+
+    console.log(key)
+fetch('http://192.168.1.10:80/api/users/'+key )
+.then(res => res.json())
+.then((apidata)=> updaterecord(apidata))
+.catch((error)=>{
+    console.log(error)
+})
+
+}
+
+    function updaterecord(id)
+{
+    let value=id.data[0];
+    let i=id.data[0].id;
+    console.log(id);
+    let form="";
+    
+            form +=" <form id='form_table'>";
+            form +="<label for=''>Name &nbsp;&nbsp;&nbsp;&nbsp;</label>";
+            form +=`<input type='text' name='name' id='name' value="${value.name}" class='input_post_form' placeholder='' ><br><br>`;
+
+            form +="<label for=''>Email Id </label>";
+            form +="<input type='text' name='email' id='email' value="+value.email+" class='input_post_form' placeholder=''><br><br>";
+
+            form +="<label for=''>Address&nbsp;</label>";
+            form +="<input type='text' name='address' id='address' value="+value.address+" class='input_post_form'><br><br>";
+
+            form +="<label for=''>Phone &nbsp;&nbsp;&nbsp;</label>";
+            form +="<input type='text' name='phone' id='phone' value="+value.phone+" class='input_post_form'><br><br>";
+
+            form +="<label for=''>Gender&nbsp;&nbsp;</label>";
+            form +="<input type='text' name='gender' id='gender' value="+value.gender+" class='input_post_form'><br><br>";
+
+            form +="<label for=''>Country&nbsp;</label>";
+            form +="<input type='text' name='country' id='country' value="+value.country+" class='input_post_form'><br><br>";
+
+            form +="<label for=''>state  &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</label>";
+            form +="<input type='text' name='state' id='state' value="+value.state+" class='input_post_form'><br><br>";
+            form +="<input type='submit' name='name' id='' value='submit'>"
+            form +="</form>";
+            
+            document.getElementById("body").innerHTML=form;
+        form_table.onsubmit = async (e) => {
+            e.preventDefault();
+            
+             let name=document.getElementById("name").value;
+             let email=document.getElementById("email").value;
+             let address=document.getElementById("address").value;
+             let phone=document.getElementById("phone").value;
+            let gender=document.getElementById("gender").value;
+             let country=document.getElementById("country").value;
+             let state=document.getElementById("state").valdataue;
+
+            alert(name);
+
+           let response = await fetch('http://192.168.1.10:80/api/user/update/'+i,{ method: 'PUT' ,  body: JSON.stringify({ 
+             "name":name,
+            "email":email,
+           "address" : address,
+            "phone":phone,
+            "gender":gender,
+         "country" :country,
+            "state": state
+        }),
+             headers: { 
+                "Content-type": "application/json;"
+            } })
+
+             let result = await response.json();
+
+            alert(result.message);
+         };
+
+       
+}
+
+
+
+
+
+
+
+
+//     let change=res[id].id;
+//     fetch(' http://192.168.1.10:80/api/user/update/5'+change,{method: 'PUT'})
+//     .then ((response)=>response.json())
+//     .then ((data)=>console.log(data))
+//     .catch(function (err){
+//         console.log(err);
     
 
-    });
-}
+//     });
+// }
